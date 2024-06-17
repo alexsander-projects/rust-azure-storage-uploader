80240905fd730b189d221684fc9f3045eee4a2cf
Alexsander-j <alexsanderjuan13@gmail.com>
2024-06-17 12:25:18 -0300

    gui version --

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use azure_storage::prelude::*;
 use azure_storage_blobs::prelude::*;
 use std::fs;
 use std::io::Read;
-use tokio::runtime::Runtime;
-use druid::{AppLauncher, Widget, WindowDesc, widget::{TextBox, Button, Flex, Label}, Data, Lens, Env, EventCtx, Command, Target, WidgetExt};
-use druid::commands::QUIT_APP;
-use std::path::{Path, PathBuf};
+use druid::{
+    AppLauncher, Widget, WindowDesc,
+    widget::{TextBox, Button, Flex, Label},
+    Data, Lens, Env, Command, Target, WidgetExt, ExtEventSink, AppDelegate, DelegateCtx, Handled, Selector,
+};
+use std::path::Path;
 use futures::future::try_join_all;
 use indicatif::{ProgressBar, ProgressStyle};
 
@@ -17,11 +19,18 @@ struct Uploader {
     storage_account: String,
     storage_account_key: String,
     info: String,
+    progress: String,
 }
+
+const UPDATE_PROGRESS: Selector<String> = Selector::new("uploader.update-progress");
 
 fn ui_builder() -> impl Widget<Uploader> {
     let info_label = Label::new(|data: &Uploader, _env: &Env| {
         format!("{}", data.info)
+    });
+
+    let progress_label = Label::new(|data: &Uploader, _env: &Env| {
+        format!("{}", data.progress)
     });
 
     let container_input = Flex::row()
@@ -44,65 +53,61 @@ fn ui_builder() -> impl Widget<Uploader> {
         .with_child(Label::new("Storage Account Key:"))
         .with_child(TextBox::new().lens(Uploader::storage_account_key));
 
-    let upload_button = Button::new("Upload").on_click(move |_ctx, data: &mut Uploader, _env| {
+    let upload_button = Button::new("Upload").on_click(move |ctx, data: &mut Uploader, _env| {
         let container = data.container.clone();
         let folder_path = data.folder_path.clone();
         let upload_folder = data.upload_folder.clone();
         let account = data.storage_account.clone();
         let access_key = data.storage_account_key.clone();
+        let ext_event_sink = ctx.get_external_handle();
+
         data.info = format!("Uploading files from {} to {}/{} in container {}...", folder_path, account, upload_folder, container);
 
-        tokio::spawn(async move {
-            let account = std::env::var("STORAGE_ACCOUNT").expect("Set env variable STORAGE_ACCOUNT first!");
-            let access_key = std::env::var("STORAGE_ACCESS_KEY").expect("Set env variable STORAGE_ACCESS_KEY first!");
+        std::thread::spawn(move || {
+            let rt = tokio::runtime::Runtime::new().expect("Failed to create Tokio runtime");
+            rt.block_on(async move {
+                let storage_credentials = StorageCredentials::access_key(account.clone(), access_key);
+                let blob_service_client = BlobServiceClient::new(account, storage_credentials);
 
-            let storage_credentials = StorageCredentials::access_key(account.clone(), access_key);
-            let blob_service_client = BlobServiceClient::new(account, storage_credentials);
+                let entries: Vec<_> = fs::read_dir(Path::new(&folder_path)).expect("Directory not found").collect();
+                let mut upload_futures = vec![];
 
-            let entries: Vec<_> = fs::read_dir(Path::new(&folder_path)).expect("Directory not found").collect();
+                let pb = ProgressBar::new(entries.len() as u64);
+                pb.set_style(ProgressStyle::default_bar()
+                    .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})")
+                    .expect("Failed to set progress bar template")
+                    .progress_chars("#>-"));
 
+                for entry in entries {
+                    let entry = entry.expect("Failed to read entry");
+                    let path = entry.path();
+                    if path.is_file() {
+                        let file_name = path.file_name().expect("Failed to get file name").to_str().expect("Failed to convert to str");
+                        let blob_name = format!("{}/{}", upload_folder, file_name);
 
-            let mut upload_futures = vec![];
+                        let mut file = fs::File::open(&path).expect("Failed to open file");
+                        let mut data = Vec::new();
+                        file.read_to_end(&mut data).expect("Failed to read file");
 
-            let pb = ProgressBar::new(entries.len() as u64);
-            pb.set_style(ProgressStyle::default_bar()
-                .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})")
-                .expect("Failed to set progress bar template")
-                .progress_chars("#>-"));
+                        let blob_client = blob_service_client
+                            .container_client(&container)
+                            .blob_client(&blob_name);
 
-            for entry in entries {
-                let entry = entry.expect("Failed to read entry");
-                let path = entry.path();
-                if path.is_file() {
-                    let file_name = path.file_name().expect("Failed to get file name").to_str().expect("Failed to convert to str");
-                    let blob_name = format!("{}/{}", upload_folder, file_name);
+                        let task = blob_client.put_block_blob(data).into_future();
+                        upload_futures.push(task);
 
-                    let mut file = fs::File::open(&path).expect("Failed to open file");
-                    let mut data = Vec::new();
-                    file.read_to_end(&mut data).expect("Failed to read file");
+                        pb.inc(1);
 
-                    let blob_client = blob_service_client
-                        .container_client(&container)
-                        .blob_client(&blob_name);
-
-                    let task = blob_client.put_block_blob(data).into_future();
-                    upload_futures.push(task);
-
-                    pb.inc(1);
-
-                    println!("Started uploading {} to {}", file_name, blob_name);
-
-                    // Finish the progress bar
-                    pb.finish_with_message("All files uploaded successfully!");
-
-
+                        ext_event_sink.submit_command(UPDATE_PROGRESS, format!("Started uploading {} to {}", file_name, blob_name), Target::Auto).unwrap();
+                    }
                 }
-            }
 
-            // Allow all blobs to upload.
-            try_join_all(upload_futures).await.expect("Failed to upload files");
+                // Allow all blobs to upload.
+                try_join_all(upload_futures).await.expect("Failed to upload files");
 
-            println!("All files uploaded successfully!");
+                pb.finish_with_message("All files uploaded successfully!");
+                ext_event_sink.submit_command(UPDATE_PROGRESS, "All files uploaded successfully!".to_string(), Target::Auto).unwrap();
+            });
         });
     });
 
@@ -114,6 +119,7 @@ fn ui_builder() -> impl Widget<Uploader> {
         .with_child(storage_account_key_input)
         .with_child(upload_button)
         .with_child(info_label)
+        .with_child(progress_label)
 }
 
 fn main() {
@@ -127,12 +133,23 @@ fn main() {
         storage_account: String::new(),
         storage_account_key: String::new(),
         info: String::new(),
+        progress: String::new(),
     };
 
-    let rt = Runtime::new().unwrap();
-    rt.block_on(async {
-        AppLauncher::with_window(main_window)
-            .launch(initial_state)
-            .expect("Failed to launch application");
-    });
+    AppLauncher::with_window(main_window)
+        .delegate(Delegate)
+        .launch(initial_state)
+        .expect("Failed to launch application");
+}
+
+struct Delegate;
+
+impl AppDelegate<Uploader> for Delegate {
+    fn command(&mut self, _: &mut DelegateCtx, _: Target, cmd: &Command, data: &mut Uploader, _: &Env) -> Handled {
+        if let Some(progress) = cmd.get(UPDATE_PROGRESS) {
+            data.progress = progress.clone();
+            return Handled::Yes;
+        }
+        Handled::No
+    }
 }
